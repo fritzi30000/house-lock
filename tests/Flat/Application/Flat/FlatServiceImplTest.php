098c3c083268ba7a2f3333782a099eb6e4e985ec
maja.chlodnicka <maja.chlodnicka@displate.com>
2023-06-13 21:32:08 +0200

    test(flat): create test cases to implement

--- tests/Flat/Application/Flat/FlatServiceImplTest.php
+++ tests/Flat/Application/Flat/FlatServiceImplTest.php
@@ -8,9 +8,15 @@ use HouseLock\Flat\Infrastructure\FlatInMemoryRepository;
 use HouseLock\Tests\Flat\Application\Command\CreateFlatObjectMother;
 use PHPUnit\Framework\TestCase;
 
+/**
+ * @covers \HouseLock\Flat\Application\Service\FlatServiceImpl
+ */
 class FlatServiceImplTest extends TestCase
 {
-    public function testShouldCreateFlat(): void
+    /**
+     * @test
+     */
+    public function should_create_flat(): void
     {
         // Given
         $userId = 1;
@@ -25,7 +31,10 @@ class FlatServiceImplTest extends TestCase
         self::assertSame(0, $result->id);
     }
 
-    public function testShouldNotCreateFlatBecauseItAlreadyExistsUnderAddress(): void
+    /**
+     * @test
+     */
+    public function should_not_create_flat_because_it_already_exists_under_address(): void
     {
         // Given
         $userId = 1;
@@ -36,9 +45,162 @@ class FlatServiceImplTest extends TestCase
 
         // Expects
         $this->expectException(FlatException::class);
-        $this->expectExceptionMessage("You have already added flat under such address: {$flatPayload->address->toString()}");
+        $this->expectExceptionMessage(
+            "You have already added flat under such address: {$flatPayload->address->toString()}"
+        );
 
         // When
         $service->create($userId, $flatPayload);
     }
+
+    /**
+     * @test
+     */
+    public function should_update_flat(): void
+    {
+        // Expects
+
+        // Given
+
+        // When
+
+        //Then
+
+    }
+
+    /**
+     * @test
+     */
+    public function should_not_update_non_existing_flat(): void
+    {
+        // Expects
+
+        // Given
+
+        // When
+
+        //Then
+
+    }
+
+    /**
+     * @test
+     */
+    public function should_update_flats_address(): void
+    {
+        // Expects
+
+        // Given
+
+        // When
+
+        //Then
+
+    }
+
+    /**
+     * @test
+     */
+    public function should_not_update_flats_address_if_other_flat_with_the_same_address_exists(): void
+    {
+        // Expects
+
+        // Given
+
+        // When
+
+        //Then
+
+    }
+
+    /**
+     * @test
+     */
+    public function should_update_flats_capacity(): void
+    {
+        // Expects
+
+        // Given
+
+        // When
+
+        //Then
+
+    }
+
+    /**
+     * @test
+     */
+    public function should_not_set_flat_capacity_less_than_current_number_of_tenants(): void
+    {
+        // Expects
+
+        // Given
+
+        // When
+
+        //Then
+
+    }
+
+    /**
+     * @test
+     */
+    public function should_update_flats_deposit(): void
+    {
+        // Expects
+
+        // Given
+
+        // When
+
+        //Then
+
+    }
+
+    /**
+     * @test
+     */
+    public function should_update_flats_utility_config(): void
+    {
+        // Expects
+
+        // Given
+
+        // When
+
+        //Then
+
+    }
+
+    /**
+     * @test
+     */
+    public function should_delete_flats(): void
+    {
+        // Expects
+
+        // Given
+
+        // When
+
+        //Then
+
+    }
+
+    /**
+     * @test
+     */
+    public function should_not_delete_occupied_flat(): void
+    {
+        // Expects
+
+        // Given
+
+        // When
+
+        //Then
+
+    }
+
 }
